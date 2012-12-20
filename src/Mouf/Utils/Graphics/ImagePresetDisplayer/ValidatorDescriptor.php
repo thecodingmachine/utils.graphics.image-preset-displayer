9b49808b5d7a83e0c06a503c11414f3ebecd9de7
nguyenk <k.nguyen@thecodingmachine.com>
2012-12-20 17:20:33 +0100

    Fixing Bugs

--- src/Mouf/Utils/Graphics/ImagePresetDisplayer/ValidatorDescriptor.php
+++
@@ -1,4 +0,0 @@
-<?php
-MoufAdmin::getValidatorService()->registerBasicValidator('Image Presets Displayer', 'plugins/utils/graphics/imagepresetdisplayer/1.0/direct/validate.php');
-
-?>
---
+++ src/ValidatorDescriptor.php
@@ -0,0 +1,3 @@
+<?php
+MoufAdmin::getValidatorService()->registerBasicValidator('Image Presets Displayer', 'vendor/mouf/utils.graphics.image-preset-displayer/src/direct/validate.php');
+?>
