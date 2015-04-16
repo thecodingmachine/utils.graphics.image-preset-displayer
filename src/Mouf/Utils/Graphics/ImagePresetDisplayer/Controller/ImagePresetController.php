52718237dd6c6ee5d78bb545d753edd464768d2d
ken <k.nguyen@thecodingmachine.com>
2015-04-16 17:40:40 +0200

    working...

---
+++ src/Mouf/Utils/Graphics/ImagePresetDisplayer/Controller/ImagePresetController.php
@@ -0,0 +1,19 @@
+<?php
+namespace Mouf\Utils\Graphics\ImagePresetDisplayer\Controller;
+
+use Mouf\Mvc\Splash\Controllers\Controller;
+use Mouf\Mvc\Splash\UrlEntryPoint;
+
+class ImagePresetController extends Controller{
+
+    private $url;
+
+    /**
+     * @URL {$this->url}
+     */
+    public function image(){
+
+    }
+
+
+}
